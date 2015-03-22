cde1eab3631fa4b0cd185cc35d201a6f5cb1b11a
André Valente <andrevalente@gmail.com>
2015-03-21 21:11:18 -0400

    atualizaçao final do PA2 - adição de diversos comentários no código fonte.

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,47 @@
-## Programming Assigment 2
-## André Valente do Couto
+## Programming Assignment 2  -  André Valente do Couto
 ## https://github.com/andrevalentebr/ProgrammingAssignment2
 
-## Write a short comment describing this function
+## The function makeCacheMatrix implements a matrix object with any
+## functions to set/get data and set/get inverse of parameter matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
+    # function set initialize matrix "x" and set Null to "inv"
+    # the "<<-" insert value of "y" in another environment
+    # where "x" is defined and function is calling.
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+    # return de "x" matrix in scope where the func was call    
     get <- function() x
+    # set inverse matrix "x" to matrix "inv"
     setinv <- function(inverse) inv <<- inverse
+    # return matrix "inv"
     getinv <- function() inv
-    list(set=set, get=get, setinverse=setinverse, getinv=getinv)
+    # list parameters names
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
-
-## Write a short comment describing this function
+## Computes the inverse of the special "matrix" returned by `makeCacheMatrix`.
+## If the inverse has calculated (and the matrix has not changed), then
+## `cacheSolve` should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
+    # get data from "inv" in scope of makeCacheMatrix function 
+    # and set to "inv" in this scope function
     inv <- x$getinv()
+    # if inverse has already been calculated, return only message and "inv" and exit.
     if(!is.null(inv)) {
         message("getting cached data.")
         return(inv)
     }
+    # store matrix x in "data" matrix
     data <- x$get()
+    # store inverse of "data" in "inv"
     inv <- solve(data)
+    # store the inverse matrix in "inv" in dad scope of matrixCacheMake was defined.
     x$setinv(inv)
+    # show data from "inv" matrix in this scope.
     inv
 }
